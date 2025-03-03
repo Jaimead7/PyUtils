bac15a325cc3169d82140b82bde4d833d70774ec
Jaime Alvarez Diaz <alvarez.diaz.jaime1@gmail.com>
2025-03-03 19:24:23 +0100

    Add noInstantiable tests

--- myPyUtils/src/noInstantiable.py
+++ myPyUtils/src/noInstantiable.py
@@ -1,4 +1,4 @@
-class NoInstantiable():
+class NoInstantiable:
     """Class that can't be instantiable"""
     def __new__(cls) -> None:
         raise SyntaxError(f'Class "{cls.__name__}" is not instantiable')
