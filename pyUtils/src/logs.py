9890b69e8669c2f8ae40be2139b9d53393c973cf
Jaime Álvarez Díaz <jadiaz@grupocopo.com>
2025-04-04 14:23:19 +0200

    Improve logging.Formatter

--- pyUtils/src/logs.py
+++ pyUtils/src/logs.py
@@ -1,7 +1,5 @@
 import logging
 
-from .config import cfg
-
 
 class Styles:
     ENDC = '\033[0m'
@@ -19,46 +17,38 @@ class Styles:
 
 
 class _MyFormatter(logging.Formatter):
-    FORMATS: dict = {
-        logging.DEBUG: Styles.DEBUG + '%(levelname)s -----> %(asctime)s:' + Styles.ENDC + ' %(message)s',
-        logging.INFO: Styles.INFO + '%(levelname)s ------> %(asctime)s:' + Styles.ENDC + ' %(message)s',
-        logging.WARNING: Styles.WARNING + '%(levelname)s ---> %(asctime)s:' + Styles.ENDC + ' %(message)s',
-        logging.ERROR: Styles.ERROR + '%(levelname)s -----> %(asctime)s:' + Styles.ENDC + ' %(message)s',
-        logging.CRITICAL: Styles.CRITICAL + '%(levelname)s --> %(asctime)s:' + Styles.ENDC + ' %(message)s',
-    }
-
     def format(self, record) -> str:
-        log_fmt: str = self.FORMATS.get(record.levelno)
+        try:
+            customStyle: str = str(record.customStyle)
+        except AttributeError:
+            customStyle = Styles.ENDC
+        arrow: str = '-' * (10-len(record.levelname)) + '>'
+        log_fmt: str = f'{customStyle}{record.levelname} {arrow} %(asctime)s:{Styles.ENDC} {record.msg}'
         formatter = logging.Formatter(log_fmt, datefmt='%d/%m/%Y %H:%M:%S')
         return formatter.format(record)
 
 
-def _getLoggingLevel() -> int:
-    try:
-        return logging.__dict__[cfg.app.loggingLevel.upper()]
-    except (KeyError, AttributeError):
-        return logging.DEBUG
-
-def setLoggingLevel(lvl: int = _getLoggingLevel()) -> int:
+def setLoggingLevel(lvl: int = logging.DEBUG) -> int:
+    _logger: logging.Logger = logging.getLogger(__name__)
     _logger.setLevel(lvl)
-print(__name__)
-_logger: logging.Logger = logging.getLogger(__file__)
+
+_logger: logging.Logger = logging.getLogger(__name__)
 setLoggingLevel()
 _streamHandler = logging.StreamHandler()
 _streamHandler.setFormatter(_MyFormatter())
 _logger.addHandler(_streamHandler)
 
-def debugLog(msg: str, style: Styles = Styles.ENDC) -> None:
-    _logger.debug(f'{style}{msg}{Styles.ENDC}')
+def debugLog(msg: str, style: Styles = Styles.DEBUG) -> None:
+    _logger.debug(f'{msg}', extra= {'customStyle': style})
 
-def infoLog(msg: str, style: Styles = Styles.ENDC) -> None:
-    _logger.info(f'{style}{msg}{Styles.ENDC}')
+def infoLog(msg: str, style: Styles = Styles.INFO) -> None:
+    _logger.info(f'{msg}', extra= {'customStyle': style})
 
-def warningLog(msg: str, style: Styles = Styles.ENDC) -> None:
-    _logger.warning(f'{style}{msg}{Styles.ENDC}')
+def warningLog(msg: str, style: Styles = Styles.WARNING) -> None:
+    _logger.warning(f'{msg}', extra= {'customStyle': style})
 
-def errorLog(msg: str, style: Styles = Styles.ENDC) -> None:
-    _logger.error(f'{style}{msg}{Styles.ENDC}')
+def errorLog(msg: str, style: Styles = Styles.ERROR) -> None:
+    _logger.error(f'{msg}', extra= {'customStyle': style})
 
-def criticalLog(msg: str, style: Styles = Styles.ENDC) -> None:
-    _logger.critical(f'{style}{msg}{Styles.ENDC}')
+def criticalLog(msg: str, style: Styles = Styles.CRITICAL) -> None:
+    _logger.critical(f'{msg}', extra= {'customStyle': style})
