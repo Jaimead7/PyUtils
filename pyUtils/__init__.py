0a98f2a1a718251a8c369fdd8dbc461eb5f9ea97
Jaime Álvarez Díaz <alvarez.diaz.jaime1@gmail.com>
2025-09-17 09:48:53 +0200

    Add filesystem

--- pyUtils/__init__.py
+++ pyUtils/__init__.py
@@ -2,6 +2,11 @@ import logging
 from pathlib import Path
 
 from .src.config import ConfigDict, ConfigFileManager, ProjectPathsDict
+from .src.filesystem import (ConfigFileValidator, ImageFileValidator,
+                             MyDirValidator, MyFileValidator,
+                             TomlFileValidator, TxtFileValidator,
+                             YamlFileValidator, copy_files,
+                             my_file_validator_factory, unzip_dir)
 from .src.logs import MyLogger, Styles, my_logger
 from .src.no_instantiable import NoInstantiable
 from .src.timing import time_me
